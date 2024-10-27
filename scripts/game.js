34d9acea8c5042cf7ed877db4dac0ace6faeb4cb
Coderback <cavinekevin514@gmail.com>
2024-10-27 19:01:03 +0300

    player name change

--- scripts/game.js
+++ scripts/game.js
@@ -19,7 +19,7 @@ function resetGameStatus() {
 }
 
 function startNewGame() {
-  if (players[0].name === "" || players[1].name == "") {
+  if (players[0].name === "" || players[1].name === "") {
     alert("Please set custom names for both players!");
     return;
   }
